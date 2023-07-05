bdc3d196db32c404e9f8c16b73b12bf418ef0038
josh roberts <josh.roberts0527@gmail.com>
2023-07-05 16:31:42 +0000

    07/05/23.11:31

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -18,6 +18,22 @@ function triangles() {
 ////////////////////////////////////////////////////////////////////////////////
 
 function fizzBuzz() {
+  // for loop starts at 1, ends at but  includes 15, increments 1 
+  for(let i = 1; i <= 15 ; i++){
+    // conditional statment with multiples of 3 & 5
+    if (i % 3 === 0 && i % 5 === 0){
+      console.log('fizzbuzz');
+    }//conditional statment with multiples of 3
+    else if (i % 3 === 0){
+      console.log('fizz');
+    }// conditional statment with multiples of 5
+    else if( i % 5 ===0){
+      console.log('buzz')
+    }//what to do if not any above condition
+    else{
+      console.log(i)
+    }
+  }
   
 }
 
@@ -25,10 +41,24 @@ function fizzBuzz() {
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function drawChessboard() {
-
+function drawChessboard(size) {
+  var size = 8;
+  // init variable myBoard
+  var myBoard = '';
+  //for loop that begins at 0, end at 8 and increments 1 that represents the board
+  for(let i = 0; i < size; i++){
+    // nested for loop that represents the rows
+    for(let j = 0; j < 8; j++){
+      if ((i + j) % 2 === 0){
+          myBoard += '';
+      }else {
+          myBoard += '#';
+      }
+    }
+  }
+      myBoard += '\n'
 }
-
+console.log(myBoard);
 ////////////////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
